3fc15f50e5629aba77e5efdab5cea4f02144f91a
Mitchell Deane <mitchelld20@yahoo.com>
2017-02-06 12:26:21 -0600

    added Template helpers and events with comments

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,2 +1,19 @@
+import {Template} from 'meteor/templating';
+
 import "./body.html";
-import "./test.html";
+
+//Template is for using the Template function. body is referring to body.html. Helpers accepts an object
+//A helper takes data and sets it up for reference in the specified file - Template. (body.html) .helpers
+//Each of its properties can be referenced for placing their key on the page.
+//When using 'helpers', you can just use {{hello}} instead of {{>hello}} 
+Template.body.helpers({     
+	hello: "Helllooooo!"
+});
+
+
+//events when you click an element with the 'test' class, this event will trigger a function - this console.log
+Template.body.events({
+	'click .test': function(event){
+		console.log('Heyyyy. You clicked me!!!')
+	}
+})
